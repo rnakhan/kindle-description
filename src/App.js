6b4bfff2c27e6d39a1971dc8790eccfb70be0cc8
Nasir Khan <rnakhan@gmail.com>
2021-04-04 17:57:48 -0700

    v1 complete. Deploy also done and embedded in main site.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import 'semantic-ui-css/semantic.min.css';
-import { Container, Header, Divider } from 'semantic-ui-react';
+import { Container, Header, Divider, Icon, Segment } from 'semantic-ui-react';
 import { Editor } from '@tinymce/tinymce-react';
 
 import HtmlSrcPanel from './HtmlSrcPanel';
@@ -16,18 +16,37 @@ const App = () => {
   return (
     <Container>
       <Container fluid>
-        <Header as="h2">Header</Header>
+        <Header as="h2">Amazon KDP Book Description Editor</Header>
         <p>
-          This is the best form of Amazon bah blah and you van get the exact
-          mechanism in place for this when you need it.
+          Amazon has strict rules about writing your book description and even
+          if you know HTML you may be in for a surprise because Amazon only
+          allows a restricted set of HTML tags.
         </p>
+        <p>
+          While there are some book description tools that may just do the job
+          for you - we guarantee that we generate the most optimal minified
+          version of HTML output. Our HTML generator uses 30% less characters
+          than most editors, thereby giving you most of the 4000 characters to
+          write your book description.
+        </p>
+        <Segment raised>
+          <p>
+            Don't believe us? Just start writing your book description content
+            and see the optimized Amazon compliant code in realtime below.
+          </p>
+        </Segment>
       </Container>
-      <Divider />
+      <Divider horizontal>
+        <Header as="h4">
+          <Icon name="edit" />
+          Editor
+        </Header>
+      </Divider>
       <Editor
-        apiKey="zdnxg7ujvq650ulbo75wod1y4gs68hb84tzg3z8bn3pzol06"
+        apiKey={process.env.REACT_APP_MINI_PROD_API_KEY}
         init={{
           menubar: 'edit',
-          height: 600,
+          height: 500,
           plugins: [
             'advlist autolink lists link image charmap print preview anchor',
             'searchreplace visualblocks code fullscreen',
--- src/HtmlSrcPanel.js
+++ src/HtmlSrcPanel.js
@@ -1,5 +1,15 @@
-import React from 'react';
-import { Grid } from 'semantic-ui-react';
+import React, { useState, useEffect } from 'react';
+import {
+  Grid,
+  Divider,
+  Icon,
+  Header,
+  Statistic,
+  Label,
+  Card,
+  Button,
+  Modal,
+} from 'semantic-ui-react';
 
 const htmlMinifier = (inp) => {
   const scrubbed = inp
@@ -21,16 +31,99 @@ const htmlMinifier = (inp) => {
   return scrubbed;
 };
 
+const statColor = (count) => (count <= 4000 ? 'green' : 'red');
+
 const HtmlSrcPanel = ({ html }) => {
+  // copied has three states DEFAULT, YES, NO
+  const [copied, setCopied] = useState('DEFAULT');
+  const [showModal, setShowModal] = useState(false);
+  useEffect(() => {
+    if (copied == 'YES' || copied == 'NO') {
+      setShowModal(true);
+    }
+  }, [copied]);
   const minifiedHtml = htmlMinifier(html);
-  const length = minifiedHtml.length;
+  const length = minifiedHtml.length > 0 ? minifiedHtml.length + 1 : 0;
+  const closeModal = () => {
+    setCopied('DEFAULT');
+    setShowModal(false);
+  };
   return (
-    <Grid container rows={2} divided="vertically">
-      <Grid.Row>
-        <div>{minifiedHtml}</div>
-      </Grid.Row>
-      <Grid.Row>{length}</Grid.Row>
-    </Grid>
+    <>
+      <Grid container rows={3}>
+        <Grid.Row textAlign="right">
+          <Grid.Column floated="right" style={{ marginTop: 5, marginRight: 0 }}>
+            <Label>
+              <Statistic horizontal size="mini" color={statColor(length)}>
+                <Statistic.Label>
+                  Characters&nbsp;&nbsp;&nbsp;&nbsp;
+                </Statistic.Label>
+                <Statistic.Value>{length}</Statistic.Value>
+                <Statistic.Label> / 4000 </Statistic.Label>
+              </Statistic>
+            </Label>
+          </Grid.Column>
+        </Grid.Row>
+
+        <Grid.Row textAlign="center">
+          <Grid.Column>
+            <Button
+              primary
+              disabled={length == 0}
+              animated="fade"
+              onClick={() => {
+                navigator.clipboard.writeText(minifiedHtml).then(
+                  () => {
+                    setCopied('YES');
+                  },
+                  () => {
+                    setCopied('NO');
+                  }
+                );
+              }}
+            >
+              <Button.Content visible>
+                <Icon name="copy" /> Copy output
+              </Button.Content>
+              <Button.Content hidden>Click to copy code</Button.Content>
+            </Button>
+          </Grid.Column>
+        </Grid.Row>
+        <Divider horizontal>
+          <Header as="h4">
+            <Icon name="file code outline" />
+            Output
+          </Header>
+        </Divider>
+        <Grid.Row>
+          <Card fluid>
+            <Card.Content>{minifiedHtml}</Card.Content>
+          </Card>
+        </Grid.Row>
+      </Grid>
+      <Modal size="mini" open={showModal} onClose={closeModal}>
+        <Modal.Header>
+          {copied == 'YES' ? 'Code Copied' : 'Unable to copy'}
+        </Modal.Header>
+        <Modal.Content>
+          {copied == 'YES' ? (
+            <p>
+              You may now paste this code in the Amazon book description form
+              without any alteration
+            </p>
+          ) : (
+            <p>
+              Please try copying by selecting the code below and using Ctrl-C or
+              Cmd-C, then paste this code in the Amazon book description form
+              without any alteration{' '}
+            </p>
+          )}
+        </Modal.Content>
+        <Modal.Actions>
+          <Button onClick={closeModal}>OK</Button>
+        </Modal.Actions>
+      </Modal>
+    </>
   );
 };
 
